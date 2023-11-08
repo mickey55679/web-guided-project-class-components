b15a1faf0e616f8bd59f0974776717890bbb39bc
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-08 13:32:47 -0600

    working on it

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -47,8 +47,17 @@ class App extends React.Component {
   }
 // in classes we have properties and methods, so you can just do this...
   addItem = (e, item) => {
-    e.preventDefaults(); // this makes sure that if we click on something it doesn't refresh the page.
+    e.preventDefault(); // this makes sure that if we click on something it doesn't refresh the page.
+    const newItem = {
+      name: item, // item that we passed into the addItem method 
+      id: Date.now(), // gives us a different, random number every single time, this way all the IDs for our items are different
+      purchased: false // because when we are adding an item to our list we don't want it to be purchased right away
+    }
+    //now we need to update our state
+    this.setState({...this.state, groceries: [...this.state.groceries, newItem] }) // this will overwrite our whole state object ... makes copy, with this.state we don't actually have to use the spread operator if we do not want to. 
+    // we also overwrite groceries, and they are equal to what the groceries were, but we are also adding in our new item as well with the `newItem`
   }
+
   // Class methods to update state
   render() {
     return (
@@ -57,8 +66,9 @@ class App extends React.Component {
            <h1>Shopping List</h1>
            <ListForm />
          </div>
-        <GroceryList groceries={groceries} />
+        <GroceryList groceries={this.state.groceries} />
         <button className="clear-btn">Clear Purchased</button>
+        {/* <button onClick={(e) => this.addItem(e, 'orange')}>Add orange</button> */ }
        </div>
     );
   }
